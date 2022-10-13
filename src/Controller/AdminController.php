597c59246ebb0fbaf1fd064c4f1cbd61c093a286
DubDubToinou <dubuc.ant@gmail.com>
2022-10-13 13:08:30 +0200

    Ajout Admin controller + lien nav + controle dans le security pour acceder uniquement au role admin

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * @Route("/Admin")
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/Panel", name="admin_panel")
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
